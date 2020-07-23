472fb45e73c81b68d579f9eedee5f327779decce
Hai Ly <hailyna93@gmail.com>
2020-07-23 15:19:27 +0700

    slider page

--- lib/pages/slider_page.dart
+++ lib/pages/slider_page.dart
@@ -13,28 +13,89 @@ class _SliderPageState extends State<SliderPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child: PageView(
-          controller: _pageController,
-          onPageChanged: (int page) {
-            _currentPage = page;
-            setState(() {});
-          },
+        child: Stack(
           children: <Widget>[
-            buildPageContent(
-                image: "assets/images/onboarding1.png",
-                color: Color(0xFFFF7252),
-                body: "Browse the menu and order directly from the application",
-                isShowImgTop: false),
-            buildPageContent(
-                image: "assets/images/onboarding2.png",
-                color: Colors.orange,
-                body: "Browse the menu and order directly from the application",
-                isShowImgTop: false),
-            buildPageContent(
-                image: "assets/images/onboarding3.png",
-                color: Colors.blue,
-                body: "Browse the menu and order directly from the application",
-                isShowImgTop: false)
+            PageView(
+              controller: _pageController,
+              onPageChanged: (int page) {
+                _currentPage = page;
+                setState(() {});
+              },
+              children: <Widget>[
+                buildPageContent(
+                    image: "assets/images/onboarding1.png",
+                    color: Color(0xFFFF7252),
+                    body:
+                        "Browse the menu and order directly from the application",
+                    isShowImgTop: false),
+                buildPageContent(
+                    image: "assets/images/onboarding2.png",
+                    color: Colors.orange,
+                    body:
+                        "Browse the menu and order directly from the application",
+                    isShowImgTop: true),
+                buildPageContent(
+                    image: "assets/images/onboarding3.png",
+                    color: Colors.blue,
+                    body:
+                        "Browse the menu and order directly from the application",
+                    isShowImgTop: false)
+              ],
+            ),
+            Positioned(
+              bottom: 40,
+              left: MediaQuery.of(context).size.width * .05,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    width: MediaQuery.of(context).size.width * .9,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        for (int i = 0; i < _totalPages; i++)
+                          i == _currentPage
+                              ? _buildPageIndicator(true)
+                              : _buildPageIndicator(false),
+                        Spacer(),
+                        if (_currentPage != 2)
+                          InkWell(
+                            onTap: () {
+                              _pageController.animateToPage(2,
+                                  duration: Duration(milliseconds: 400),
+                                  curve: Curves.linear);
+                              setState(() {});
+                            },
+                            child: Text(
+                              "Skip",
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w600,
+                                  color: Colors.white),
+                            ),
+                          ),
+                        if (_currentPage == 2)
+                          InkWell(
+                            onTap: () {
+                              _pageController.animateToPage(2,
+                                  duration: Duration(milliseconds: 400),
+                                  curve: Curves.linear);
+                              setState(() {});
+                            },
+                            child: Text(
+                              "Start",
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w600,
+                                  color: Colors.white),
+                            ),
+                          )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            )
           ],
         ),
       ),
@@ -66,6 +127,18 @@ class _SliderPageState extends State<SliderPage> {
           )
         ],
       ),
+    );
+  }
+
+  Widget _buildPageIndicator(bool isCurrentPage) {
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 350),
+      margin: EdgeInsets.symmetric(horizontal: 5),
+      height: isCurrentPage ? 18 : 10,
+      width: isCurrentPage ? 18 : 10,
+      decoration: BoxDecoration(
+          color: isCurrentPage ? Colors.white : Colors.white54,
+          borderRadius: BorderRadius.circular(12)),
     );
   }
 }
